613c40bcce10be459e93b6db7a9f709eaa9747dd
MinexAutomation <minex.automation@gmail.com>
2019-10-15 14:04:24 -0700

    Added remote testing utilities.

---
+++ source/R5T.Gepidia.Remote.Testing/Code/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Gepidia.Remote.Testing
+{
+    public static class Utilities
+    {
+        public static string GetRemoteTestingRootDirectoryPath(IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            // Get user from secrets directory.
+
+            var userDirectoryPath = PathHelper.UserProfileDirectoryPathValue;
+
+            var randomDirectoryName = PathHelper.GetRandomDirectoryName();
+
+            var rootDirectoryPath = stringlyTypedPathOperator.GetDirectoryPath(userDirectoryPath, randomDirectoryName);
+            return rootDirectoryPath;
+        }
+    }
+}
