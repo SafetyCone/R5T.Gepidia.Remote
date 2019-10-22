4c0213a828d40021b18b855a0691b15fa7d78b4c
MinexAutomation <minex.automation@gmail.com>
2019-10-22 12:07:17 -0700

    Added SftpFile extension methods to get a directory-indicated path if the SftpFile is a directory.

--- source/R5T.Gepidia.Remote/Code/Extensions/SftpFileExtensions.cs
+++ source/R5T.Gepidia.Remote/Code/Extensions/SftpFileExtensions.cs
@@ -2,6 +2,8 @@
 
 using Renci.SshNet.Sftp;
 
+using R5T.Lombardy;
+
 
 namespace R5T.Gepidia.Remote.Extensions
 {
@@ -17,6 +19,27 @@ namespace R5T.Gepidia.Remote.Extensions
             {
                 return FileSystemEntryType.File;
             }
+        }
+
+        /// <summary>
+        /// Gets a directory-indicated path if the <see cref="SftpFile"/> is a directory.
+        /// Otherwise, just return the <see cref="SftpFile.FullName"/>.
+        /// The <see cref="SftpFile.FullName"/> does NOT return directory-indicated directory paths, but file paths are file-indicated.
+        /// </summary>
+        public static string GetPathDirectoryIndicatedIfDirectory(this SftpFile sftpFile, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            var output = sftpFile.IsDirectory ? stringlyTypedPathOperator.EnsureDirectoryPathIsDirectoryIndicated(sftpFile.FullName) : sftpFile.FullName;
+            return output;
+        }
+
+        /// <summary>
+        /// Gets a directory-indicated path if the <see cref="SftpFile"/> is a directory.
+        /// Uses <see cref="SftpFileExtensions.GetPathDirectoryIndicatedIfDirectory(SftpFile, IStringlyTypedPathOperator)"/>.
+        /// </summary>
+        public static string GetPath(this SftpFile sftpFile, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            var output = sftpFile.GetPathDirectoryIndicatedIfDirectory(stringlyTypedPathOperator);
+            return output;
         }
     }
 }
