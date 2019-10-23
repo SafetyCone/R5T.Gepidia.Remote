0ae782ee6b72c4738e5adcfe075fac535cf22469
MinexAutomation <minex.automation@gmail.com>
2019-10-23 16:07:50 -0700

    Added test of remote directory listing. Added DI-container configuration using R5T.Burgundy.

---
+++ source/R5T.Gepidia.Remote.Construction/Code/Construction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Gepidia.Remote.Construction
+{
+    public static class Construction
+    {
+        public static void SubMain()
+        {
+            Construction.TestEnumerationOfFileSystemEntryPaths();
+        }
+
+        private static void TestEnumerationOfFileSystemEntryPaths()
+        {
+            var serviceProvider = Program.GetServiceProvider();
+
+            var remoteFileSystemOperator = serviceProvider.GetRequiredService<RemoteFileSystemOperator>();
+
+            var directoryPath = @"/home/ec2-user/";
+
+            var remotePaths = remoteFileSystemOperator.EnumerateFileSystemEntryPaths(directoryPath, true).ToList();
+            foreach (var remotePath in remotePaths)
+            {
+                Console.WriteLine(remotePath);
+            }
+        }
+    }
+}
--- source/R5T.Gepidia.Remote.Construction/Code/Program.cs
+++ source/R5T.Gepidia.Remote.Construction/Code/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Burgundy.Extensions;
+
+
 namespace R5T.Gepidia.Remote.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Construction.SubMain();
+
+            //Program.SubMain();
+        }
+
+        private static void SubMain()
+        {
             Console.WriteLine("Hello World!");
+        }
+
+        public static IServiceProvider GetServiceProvider()
+        {
+            var serviceProvider = new ServiceCollection()
+                .UseSftpClientWrapper()
+                .AddSingleton<RemoteFileSystemOperator>()
+
+                .BuildServiceProvider()
+                ;
+
+            return serviceProvider;
         }
     }
 }
